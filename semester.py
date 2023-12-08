55c57e4cb1ed7ab872651ef224b21a91cf5897b3
Dmytro Vozniuk <dmytro.vozniuk@knu.com>
2023-12-08 15:39:45 +0200

    first commit

---
+++ semester.py
@@ -0,0 +1,17 @@
+# Семестрова робота
+
+with open('text.txt', 'r', encoding='utf-8') as file:
+        content = file.read()
+while True:        
+    word1 = input("Введіть слово для заміни: ")
+    if word1 not in content:
+        print(f"Слово '{word1}' не знайдено у тексті.")   
+    else:
+        word2 = input("Введіть нове слово: ")
+        content = content.replace(word1, word2)
+        break
+
+with open(r'D:\Программирование\5\Lab-works\text.txt', 'w', encoding='utf-8') as file:
+        file.write(content)
+    
+print(f"Слово '{word1}' замінено на '{word2}'.")
